b68e2fe7527468839ec41c721a66788e9a682f6f
It's BOBIK <alex.chpak@gmail.com>
2022-07-20 10:48:02 +0200

    make js bundle and upgrade css

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// 3D scroll
+
+let zSpacing = -1000,
+		lastPos = zSpacing / 5,
+		$frames = document.getElementsByClassName('frame'),
+		frames = Array.from($frames),
+		zVals = []
+
+window.onscroll = function () {
+	let top = document.documentElement.scrollTop,
+			delta = lastPos - top
+	lastPos = top;
+
+	frames.forEach(function (n, i) {
+		zVals.push((i * zSpacing) + zSpacing);
+		zVals[i] += delta * -5.5;
+		
+		let frame = frames[i],
+				transform = `translateZ(${zVals[i]}px)`,
+				opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+	});
+} 
+
+window.scrollTo(0, 1);
