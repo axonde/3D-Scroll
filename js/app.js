9ce34a304c9ff54bb2580f0ca0b68ef8853177f2
It's BOBIK <alex.chpak@gmail.com>
2022-07-20 20:59:44 +0200

    finishing work

--- js/app.js
+++ js/app.js
@@ -23,3 +23,22 @@ window.onscroll = function () {
 } 
 
 window.scrollTo(0, 1);
+
+
+// Audio
+
+let soundButton = document.querySelector('.sound-btn'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused');
+	audio.paused ? audio.play() : audio.pause();
+});
+
+window.onfocus = function () {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play();
+}
+
+window.onblur = function () {
+	audio.pause();
+}
